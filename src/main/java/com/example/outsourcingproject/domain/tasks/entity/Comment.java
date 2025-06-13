660282a4ba6aaf56908755eea64e9e20d01b3ad0
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-13 20:01:49 +0900

    feat : Comment Entity 생성

---
+++ src/main/java/com/example/outsourcingproject/domain/tasks/entity/Comment.java
@@ -0,0 +1,35 @@
+package com.example.outsourcingproject.domain.tasks.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.scheduling.config.Task;
+
+@Entity
+@Getter
+@Table
+@NoArgsConstructor
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Task task;
+
+    public Comment(Long id, String contents) {
+        this.id = id;
+        this.contents = contents;
+    }
+}
