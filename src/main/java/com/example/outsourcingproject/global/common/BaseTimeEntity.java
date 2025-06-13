de7c392c35839d4187f65cd3d324a98a532479a6
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-13 20:19:40 +0900

    feat : BaseTimeEntity 생성

--- src/main/java/com/example/outsourcingproject/global/common/BaseTimeEntity.java
+++ src/main/java/com/example/outsourcingproject/global/common/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -15,5 +16,8 @@ public class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime CreatedAt;
+
+    @LastModifiedDate
+    private LocalDateTime ModifiredAt;
 
 }
