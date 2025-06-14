8a8bdc89a489f621281e9b363d1d4ee2dbf40271
NewJKH <newsun0805@gmail.com>
2025-06-15 07:20:40 +0900

    feat: "로그 ID 추출 전략을 정의하는 IdSource enum 추가"

--- src/main/java/com/example/outsourcingproject/domain/log/entity/LogType.java
+++ src/main/java/com/example/outsourcingproject/domain/log/entity/LogType.java
@@ -1,14 +1,77 @@
 package com.example.outsourcingproject.domain.log.entity;
 
 public enum LogType {
-    ALL,
-    TASK_CREATED,
-    TASK_UPDATED,
-    TASK_DELETED,
-    TASK_STATUS_CHANGED,
-    COMMENT_CREATED,
-    COMMENT_UPDATED,
-    COMMENT_DELETED,
-    USER_LOGGED_IN,
-    USER_LOGGED_OUT
+    ALL(IdSource.NULL,IdSource.NULL),
+    // NOTE : userId 요청자 ID
+    // NOTE : targetId 대상 ID ( 테스크, 댓글, 로그인, 로그아웃 )
+     /*
+     userId = { token }
+     targetId = { response: { generatorId } }
+      */
+    TASK_CREATED(IdSource.TOKEN,IdSource.RESPONSE),
+
+    /*
+     userId = { token }
+     targetId = { request: { id } }
+     */
+    TASK_UPDATED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+
+    /*
+     userId = { token }
+     targetId = { request: { id } }
+     */
+    TASK_DELETED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+
+    /*
+     userId = { token }
+     targetId = { request: { id } }
+     */
+    TASK_STATUS_CHANGED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+
+    /*
+     userId = { token }
+     targetId = { request: { task_id} }
+     */
+    COMMENT_CREATED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+
+    /*
+     userId = { token }
+     targetId = { request : { id } }
+     */
+    COMMENT_UPDATED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+
+    /*
+     userId = { token }
+     targetId = { request : { id } }
+     */
+    COMMENT_DELETED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+
+    /*
+     userId = { token }
+     targetId = { token }
+     */
+    USER_LOGGED_IN(IdSource.TOKEN,IdSource.TOKEN),
+    /*
+     userId = { token }
+     targetId = { token }
+     */
+    USER_LOGGED_OUT(IdSource.TOKEN,IdSource.TOKEN);
+
+    private final IdSource userSource;
+    private final IdSource targetSource;
+
+    LogType(IdSource userSource, IdSource targetSource) {
+        this.userSource = userSource;
+        this.targetSource = targetSource;
+
+    }
+
+    public IdSource getUserSource() {
+        return userSource;
+    }
+
+    public IdSource getTargetSource() {
+        return targetSource;
+    }
+
 }
