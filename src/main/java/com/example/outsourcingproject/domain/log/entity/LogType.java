13b3a9d7ad5df7fca514912771165fb9c4aead05
NewJKH <newsun0805@gmail.com>
2025-06-15 08:02:55 +0900

    feat: "targetId 정의하는 Key 추가"

--- src/main/java/com/example/outsourcingproject/domain/log/entity/LogType.java
+++ src/main/java/com/example/outsourcingproject/domain/log/entity/LogType.java
@@ -1,77 +1,73 @@
 package com.example.outsourcingproject.domain.log.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum LogType {
-    ALL(IdSource.NULL,IdSource.NULL),
+    ALL(IdSource.NULL,IdSource.NULL,""),
     // NOTE : userId 요청자 ID
     // NOTE : targetId 대상 ID ( 테스크, 댓글, 로그인, 로그아웃 )
      /*
      userId = { token }
      targetId = { response: { generatorId } }
       */
-    TASK_CREATED(IdSource.TOKEN,IdSource.RESPONSE),
+    TASK_CREATED(IdSource.TOKEN,IdSource.RESPONSE,""),
 
     /*
      userId = { token }
      targetId = { request: { id } }
      */
-    TASK_UPDATED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+    TASK_UPDATED(IdSource.TOKEN,IdSource.PATH_VARIABLE,"id"),
 
     /*
      userId = { token }
      targetId = { request: { id } }
      */
-    TASK_DELETED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+    TASK_DELETED(IdSource.TOKEN,IdSource.PATH_VARIABLE,"id"),
 
     /*
      userId = { token }
      targetId = { request: { id } }
      */
-    TASK_STATUS_CHANGED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+    TASK_STATUS_CHANGED(IdSource.TOKEN,IdSource.PATH_VARIABLE,"taskId"),
 
     /*
      userId = { token }
      targetId = { request: { task_id} }
      */
-    COMMENT_CREATED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+    COMMENT_CREATED(IdSource.TOKEN,IdSource.PATH_VARIABLE,"taskId"),
 
     /*
      userId = { token }
      targetId = { request : { id } }
      */
-    COMMENT_UPDATED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+    COMMENT_UPDATED(IdSource.TOKEN,IdSource.PATH_VARIABLE,"id"),
 
     /*
      userId = { token }
      targetId = { request : { id } }
      */
-    COMMENT_DELETED(IdSource.TOKEN,IdSource.PATH_VARIABLE),
+    COMMENT_DELETED(IdSource.TOKEN,IdSource.PATH_VARIABLE,"id"),
 
     /*
      userId = { token }
      targetId = { token }
      */
-    USER_LOGGED_IN(IdSource.TOKEN,IdSource.TOKEN),
+    USER_LOGGED_IN(IdSource.TOKEN,IdSource.TOKEN,""),
     /*
      userId = { token }
      targetId = { token }
      */
-    USER_LOGGED_OUT(IdSource.TOKEN,IdSource.TOKEN);
+    USER_LOGGED_OUT(IdSource.TOKEN,IdSource.TOKEN,"");
 
     private final IdSource userSource;
     private final IdSource targetSource;
+    private final String targetKey;
 
-    LogType(IdSource userSource, IdSource targetSource) {
+    LogType(IdSource userSource, IdSource targetSource,String targetKey) {
         this.userSource = userSource;
         this.targetSource = targetSource;
-
-    }
-
-    public IdSource getUserSource() {
-        return userSource;
-    }
-
-    public IdSource getTargetSource() {
-        return targetSource;
+        this.targetKey = targetKey;
     }
 
 }
