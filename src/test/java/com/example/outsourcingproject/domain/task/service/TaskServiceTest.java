eb0bbd6e88e0c48c6ce536b71c01cda14fea3ddf
jh-01 <were1117@sm.ac.kr>
2025-06-17 18:43:16 +0900

    test: 통합 테스트 파일 추가

--- src/test/java/com/example/outsourcingproject/domain/task/service/TaskServiceTest.java
+++ src/test/java/com/example/outsourcingproject/domain/task/service/TaskServiceTest.java
@@ -16,8 +16,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import com.example.outsourcingproject.global.exception.task.TaskNotFoundException;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Collections;
 
