1f811fef70b0fe3401c3ae68d2dda2f32ed44344
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-19 12:22:01 +0900

    refactor : 요청 dto 리팩터링

--- src/main/java/com/example/outsourcingproject/domain/task/dto/CommentRequestDto.java
+++ src/main/java/com/example/outsourcingproject/domain/task/dto/CommentRequestDto.java
@@ -2,7 +2,10 @@ package com.example.outsourcingproject.domain.task.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class CommentRequestDto {
 
@@ -11,4 +14,5 @@ public class CommentRequestDto {
     public CommentRequestDto(@NotBlank String contents) {
         this.contents = contents;
     }
+
 }
